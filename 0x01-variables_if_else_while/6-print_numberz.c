97eb7d090183c523fd3ad3e832eb825dac6e50db
MikeBilbee <cmbilbee@github.com>
2023-01-29 08:36:00 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single-digit numberz of base 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 0; n++)
+putchar((n % 10) + '0');
+putchar('\n');
+return (0);
+}
