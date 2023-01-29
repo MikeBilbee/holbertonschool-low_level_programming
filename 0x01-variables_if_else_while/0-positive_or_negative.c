c3a80d7c17a1547b51edc4d48a70a3dc0afc91b8
MikeBilbee <cmbilbee@github.com>
2023-01-29 07:06:30 -0800

    0.3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * Prints rabdom number either pos or neg
+ * main - Prints rabdom number either pos or neg
  *
  * Return: always 0
  */
@@ -14,11 +14,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-   printf("%i is positive\n", n);
+printf("%i is positive\n", n);
 else if (n == 0)
-   printf("%i is zero\n", n);
+printf("%i is zero\n", n);
 else if (n < 0)
-   printf("%i is negative\n", n);
+printf("%i is negative\n", n);
 
 return (0);
 }
