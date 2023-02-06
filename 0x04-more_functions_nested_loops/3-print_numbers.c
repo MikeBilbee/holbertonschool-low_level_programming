dc9f8d78c0f93517521db849c4bcf33157b48c7a
root <MikeBilbee>
2023-02-06 11:00:16 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0-9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+int n = 0;
+for (n = 0; n < 10; n++)
+_putchar(n + '0');
+_putchar('\n');
+}
