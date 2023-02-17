a8a00e0094a40dcca6f38777d2f8c53099a48b8c
MikeBilbee <cmbilbee@gmail.com>
2023-02-17 04:07:59 -0600

    01

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,24 @@
 #include "main.h"
-int _atoi(void)
+/**
+ * _atoi - the thing
+ * @s: a pointer
+ */
+int _atoi(char *s)
 {
-  return (0);
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
