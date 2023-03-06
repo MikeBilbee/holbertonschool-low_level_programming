cc92aa690b4112fa5bb88019077807194d898ad3
MikeBilbee <cmbilbee@gmail.com>
2023-03-06 16:35:11 -0600

    0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib>
+/**
+ * array_iterator - Iterates an array
+ * @size: Array size
+ * @action: Pointer to funtion
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
