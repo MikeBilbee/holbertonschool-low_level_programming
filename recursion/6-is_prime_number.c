4b8c69b05886f280b8eb5e57f955240c4afae91e
MikeBilbee <cmbilbee@gmail.com>
2023-10-16 14:17:39 -0500

    6.1 trying something new

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,22 +1,29 @@
 #include "main.h"
 /**
- * is_prime_number - checks if a given number is prime
+ * is_prime_check - checks if a given number is prime
+ * @n: numerator
+ * @d: denominator
+ * Return: 1 if prime, otherwise 0
+*/
+int is_prime_check(int n, int d)
+{
+	if (d <= 1)
+	{
+        return 1;
+    }
+    if (n % d == 0)
+	{
+        return 0;
+    }
+    return is_prime_check(n, d - 1);
+}
+
+/**
+ * is_prime_number - Tells user if a given number is prime
  * @n: number
  * Return: 1 if prime, otherwise 0
 */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	if (n == 2 || n == 3)
-	{
-		return (1);
-	}
-	if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	}
-	return (is_prime_number(n / 2 + 1));
+	return (is_prime_check(n, n - 1));
 }
