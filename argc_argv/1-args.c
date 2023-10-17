0881c3ee79793776e987a6f750a63834b70f4e87
MikeBilbee <cmbilbee@gmail.com>
2023-10-16 23:19:29 -0500

    1.0 args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: argument
+ * @argv: argument
+ * Return: Alwyas 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
