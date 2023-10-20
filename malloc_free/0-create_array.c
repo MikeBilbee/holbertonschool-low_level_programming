064be9239ae2698c74d96156891d1d336ff88277
MikeBilbee <cmbilbee@gmail.com>
2023-10-20 12:09:12 -0500

    BETTY

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
+ * create_array - creates an array of chars, initializes with a specific char.
  * @size: size
  * @c: character
  * Return: Pointer to Array, otherwise NULL
