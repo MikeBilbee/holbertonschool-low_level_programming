984e003a9e7776b0fd276a6e7e9e6038bffe92d5
MikeBilbee <cmbilbee@gmail.com>
2023-10-23 10:09:52 -0500

    4.0 free! and making sure it compiles with 3

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * free_grid - Frees array at pointer returned by alloc_grid()
+ * @grid: array 
+ * @height: grid height
+ * Return: None
+*/
+void free_grid(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+	{
+		free(grid[index]);
+		free(grid);
+	}
+}
