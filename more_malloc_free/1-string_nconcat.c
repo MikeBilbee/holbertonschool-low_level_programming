9b000cb9b3d1fb3d23b52756931e6b9554a72550
MikeBilbee <cmbilbee@gmail.com>
2023-10-23 11:23:37 -0500

    1.0

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * string_nconcat - concats two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: Pointer to new memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new;
+	size_t len = n, index;
+
+	if (!s1)
+	{
+		s1 = "";
+	}
+
+	if (!s2)
+	{
+		s2 = "";
+	}
+
+	for (index = 0; s1[index]; index++)
+	{
+		len++;
+	}
+
+	new = malloc(sizeof(char) * (len + 1));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+	{
+		new[len++] = s1[index];
+	}
+
+	for (index = 0; s2[index] && index < n; index++)
+	{
+		new[len++] = s2[index];
+		new[len] = '\0';
+	}
+
+	return (new);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -8,8 +8,8 @@ This Project dives deeper into Dynamic Memory Allocation in C
 
 | Task | File | Description |
 | ----- | ----- | ----- |
-| 0. Trust no one | [0-malloc_checked.c]() | A function that concatenates two strings. |
-| 
+| 0. Trust no one | [0-malloc_checked.c](https://github.com/MikeBilbee/holbertonschool-low_level_programming/blob/master/more_malloc_free/0-malloc_checked.c) | A function that concatenates two strings. |
+| 1. string_nconcat | [1-string_nconcat.c]()
 
 
 
