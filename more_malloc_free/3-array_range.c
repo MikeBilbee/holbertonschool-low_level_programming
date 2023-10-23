2464cfb18d1341bca29606080659c2c1c668485e
MikeBilbee <cmbilbee@gmail.com>
2023-10-23 12:16:18 -0500

    3.0 min maxed arrays

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * array_range - Creates an array of integers
+ * @min: lowest value int
+ * @max: highest value int
+ * Return: Pointer to array
+*/
+int *array_range(int min, int max)
+{
+	int *new;
+	size_t index, size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = (max - min) + 1;
+	new = malloc(size *sizeof(*new));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	for (index = 0; index < size && min <= max; index++, min++)
+	{
+		*(new + index) = min;
+	}
+
+	return (new);
+}
