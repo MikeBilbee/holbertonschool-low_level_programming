996b9280cdab20d2c3cb7e768bad0abaca97114d
MikeBilbee <cmbilbee@gmail.com>
2023-10-24 13:45:24 -0500

    setup and 0.1 dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,11 @@
+<h1> HOLBERTON T1 Concurrent: C - Structures, typedef </h1>
+
+This Project covers structures (struct) and typedef
+
+---
+
+<h3> Repository Contents </h3>
+
+| Task | File | Description |
+| ----- | ----- | ----- |
+| 0. Poppy | [dog.h]() | Defines a new type ``struct dog`` |
---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ * Return: None
+*/
+typedef struct dog
+{
+	char *name, *owner;
+	float age;
+}
+
+
+dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
