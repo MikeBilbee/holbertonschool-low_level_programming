2a4b502516a51b5848ac5ca8cadd787538d5e4a8
MikeBilbee <cmbilbee@gmail.com>
2023-10-29 16:16:17 -0500

    3.0 main/caller function

---
+++ function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * main - prints result of simple operations
+ * @argc: string
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*index)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	index = (*get_op_func(argv[2]));
+	if (!index)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", i(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
