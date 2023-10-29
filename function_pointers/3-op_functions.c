9e65dfc992b574c445ba4ec4c9c8266bb2468c34
MikeBilbee <cmbilbee@gmail.com>
2023-10-29 16:13:34 -0500

    3.0 op functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - does addition
+ * @a: first number
+ * @b: second number
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - does subtraction
+ * @a: first number
+ * @b: second number
+ * Return: Difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - does multiplication
+ * @a: first number
+ * @b: second number
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - does division
+ * @a: first number
+ * @b: second number
+ * Return: Quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - does modulo division
+ * @a: first number
+ * @b: second number
+ * Return: Remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
