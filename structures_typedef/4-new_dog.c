d47f46c485fe84a3b91697dad9a10b47c0a71229
MikeBilbee <cmbilbee@gmail.com>
2023-10-29 14:00:33 -0500

    4.1 trying to get this to work

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,47 +11,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new;
 	int index, nNam, nOwn;
 
-	new = malloc(sizeof(*new));
+	new = malloc(sizeof(dog_t));
+	if (!new)
+		return (NULL);
+	new->age = age;
 
-	if (!new || !(name) || !(owner))
+	while (name[nNam++])
+		;
+	while (owner[nOwn++])
+		;
+	new->name = malloc(nNam);
+	if (new->name == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-
-	for (nNam = 0; name[nNam]; nNam++)
+	new->owner = malloc(nOwn);
+	if (new->owner == NULL)
 	{
-		;
-	}
-
-	for (nOwn = 0; owner[nOwn]; nOwn++)
-	{
-		;
-	}
-
-	new->name = malloc(nNam + 1);
-	new->owner = malloc(nOwn + 1);
-
-	if (!(new->name) || !(new->owner))
-	{
-		free(new->owner);
 		free(new->name);
 		free(new);
 		return (NULL);
 	}
-
 	for (index = 0; index < nNam; index++)
 	{
-		new->name[index] = name[index];
-		new->name[index] = '\0';
+		(*new).name[index] = name[index];
 	}
-
-	new->age = age;
-
 	for (index = 0; index < nOwn; index++)
 	{
-		new->owner[index] = owner[index];
-		new->owner[index] = '\0';
+		(*new).owner[index] = owner[index];
 	}
 
 	return (new);
